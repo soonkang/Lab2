2afca804f3a04be77ba9f921b35733ddf3ab8536
soonkang <soonkang.22@ichat.sp.edu.sg>
2023-04-27 16:01:09 +0800

    'edited'

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,16 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+  print("Height = " + str(height))
+  print("Weight = " + str(weight))
+weight =float(input("Weight : "))
+height =float(input("Height : "))
+
+bmi = weight/ height ** 2
+print("BMI = " + str(bmi))
+
+if bmi < 18.5 :
+  print("Under weight")
+elif (bmi <= 25):
+  print("Normal weight")
+else:
+  print("Over weight")
+
